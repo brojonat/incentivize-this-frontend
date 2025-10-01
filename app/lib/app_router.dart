1d47566d07016def81f7894f7f6edbcd8f312147
Jon Brown <brojonat@gmail.com>
2025-09-30 18:41:34 -0700

    Revert "deferred loader"
    This reverts commit 575e13465c5bdbac83798ecf0e0772a693076a19.

--- app/lib/app_router.dart
+++ app/lib/app_router.dart
@@ -1,60 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-// Import screens deferred
-import 'home_screen.dart' deferred as home_screen;
-import 'bounty_detail_screen.dart' deferred as bounty_detail_screen;
+import 'home_screen.dart';
+import 'bounty_detail_screen.dart';
 import 'bounty.dart';
-import 'about_screen.dart' deferred as about_screen;
-import 'marketing_screen.dart';
-import 'loading_indicator.dart'; // A simple loading widget
+// For wallet address access if needed directly by router/screen
+import 'about_screen.dart'; // Import the AboutScreen
+import 'marketing_screen.dart'; // Import the new marketing screen
 
 // Potentially, ApiService might be needed here if we decide to pre-fetch
 // import 'api_service.dart';
 
 final GlobalKey<NavigatorState> rootNavigatorKey = GlobalKey<NavigatorState>();
-
-// Helper widget to handle deferred loading
-class DeferredLoader extends StatefulWidget {
-  final Future<void> libraryFuture;
-  final Widget child;
-
-  const DeferredLoader(
-      {super.key, required this.libraryFuture, required this.child});
-
-  @override
-  _DeferredLoaderState createState() => _DeferredLoaderState();
-}
-
-class _DeferredLoaderState extends State<DeferredLoader> {
-  bool _isLoaded = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _loadLibrary();
-  }
-
-  Future<void> _loadLibrary() async {
-    await widget.libraryFuture;
-    if (mounted) {
-      setState(() {
-        _isLoaded = true;
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return _isLoaded
-        ? widget.child
-        : const Scaffold(
-            body: Center(
-              child: LoadingIndicator(message: 'Loading...'),
-            ),
-          );
-  }
-}
 
 final GoRouter appRouter = GoRouter(
   navigatorKey: rootNavigatorKey,
@@ -68,24 +25,18 @@ final GoRouter appRouter = GoRouter(
     ),
     GoRoute(
       path: '/bounties', // Old home screen is now here
-      pageBuilder: (context, state) => NoTransitionPage(
-        child: DeferredLoader(
-          libraryFuture: home_screen.loadLibrary(),
-          child: home_screen.HomeScreen(
-            key: const ValueKey('HomeScreen'),
-          ),
+      pageBuilder: (context, state) => const NoTransitionPage(
+        child: HomeScreen(
+          key: ValueKey('HomeScreen'),
         ),
       ),
       routes: <RouteBase>[
         GoRoute(
           path: ':bountyId', // Note: no leading '/'
           builder: (BuildContext context, GoRouterState state) {
-            return DeferredLoader(
-              libraryFuture: bounty_detail_screen.loadLibrary(),
-              child: bounty_detail_screen.BountyDetailScreen(
-                bountyId: state.pathParameters['bountyId']!,
-                initialBounty: state.extra as Bounty?,
-              ),
+            return BountyDetailScreen(
+              bountyId: state.pathParameters['bountyId']!,
+              initialBounty: state.extra as Bounty?,
             );
           },
         ),
@@ -94,10 +45,7 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: '/about', // Add the new route for AboutScreen
       builder: (BuildContext context, GoRouterState state) {
-        return DeferredLoader(
-          libraryFuture: about_screen.loadLibrary(),
-          child: about_screen.AboutScreen(),
-        );
+        return const AboutScreen();
       },
     ),
   ],
